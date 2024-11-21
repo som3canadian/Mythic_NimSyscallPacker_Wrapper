434161d86ba3d9ac277b074ec479777e26838129
som3canadian <somecanadian0@gmail.com>
2024-11-21 14:46:38 -0500

    add possibility to take zip file as input payload

--- Payload_Type/nimsyscallpacker_wrapper/mythic/builder.py
+++ Payload_Type/nimsyscallpacker_wrapper/mythic/builder.py
@@ -7,6 +7,8 @@ import tempfile
 from distutils.dir_util import copy_tree
 import base64
 import shutil
+import pathlib
+import zipfile
 
 class NimSyscallPackerWrapper(PayloadType):
     name = "nimsyscallpacker_wrapper"
@@ -166,16 +168,19 @@ class NimSyscallPackerWrapper(PayloadType):
         BuildParameter(
             name="input-file-extension",
             parameter_type=BuildParameterType.String,
-            description="Input file extension. Should match the extension of the payload you will select on the next page. (ex: exe for apollo.exe or bin for apollo.bin)",
+            description="Input file extension. Should match the extension of the payload you will select on the next page. (ex: exe for apollo.exe, bin for apollo.bin and zip)",
             default_value="exe",
         ),
     ]
+    # agent_path = pathlib.Path(".") / "nimsyscallpacker_wrapper" / "mythic"
     # agent_path = Path(".")
-    agent_icon_path = Path(".") / "mythic" / "nimsyscallpacker_wrapper.svg"
-    agent_code_path = Path(".") / "agent_code"
+    agent_path = pathlib.Path(".") / "mythic"
+    agent_icon_path = agent_path / "nimsyscallpacker_wrapper.svg"
+    agent_code_path = pathlib.Path(".") / "agent_code"
     c2_profiles = []
     build_steps = [
         BuildStep(step_name="getting_agent_payload", step_description="Get the agent payload that will be wrapped"),
+        BuildStep(step_name="unzipping_payload", step_description="Unzipping payload"),
         BuildStep(step_name="building_wrapper", step_description="Building wrapper payload"),
         BuildStep(step_name="Zip", step_description="Zipping payload")
     ]
@@ -186,6 +191,48 @@ class NimSyscallPackerWrapper(PayloadType):
         output = ""
         #
         try:
+            agent_build_path = tempfile.TemporaryDirectory(suffix=self.uuid).name
+            # shutil to copy payload files over
+            # copy_tree(str(self.agent_code_path), agent_build_path)
+            copy_tree(self.agent_code_path, agent_build_path)
+            #
+            # dummy input file (file produced by the agent)
+            source_file = "{}/SomeFile.{}".format(agent_build_path, self.get_parameter("input-file-extension"))
+            with open(str(source_file), "wb") as f:
+                f.write(base64.b64decode(self.wrapped_payload))
+            # if working_path is not null, then we have a file to work with
+            if not os.path.exists(source_file):
+                resp.build_stderr = "Failed to find the payload to wrap!"
+                return resp
+            await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
+                PayloadUUID=self.uuid,
+                StepName="getting_agent_payload",
+                StepStdout="We found the payload to wrap!",
+                StepSuccess=True
+            ))
+
+            # check if input-file-extension is a zip file. If so, extract the zip file
+            if self.get_parameter("input-file-extension") == "zip":
+                # create temp directory called agent_build_temp
+                agent_build_temp = tempfile.TemporaryDirectory(suffix=self.uuid).name
+                # extract zip file to temp directory
+                with zipfile.ZipFile(source_file, 'r') as zip_ref:
+                    zip_ref.extractall(agent_build_path)
+                # check if extracted zip file contains a .exe file. If so, set input-file-extension to exe and copy the exe file to agent_code_path
+                for file in os.listdir(agent_build_path):
+                    if file.endswith(".exe"):
+                        source_file = "{}/{}".format(agent_build_path, file)
+                        shutil.copy(source_file, self.agent_code_path)
+                        self.get_parameter("input-file-extension") == "exe"
+                        break
+
+                await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
+                    PayloadUUID=self.uuid,
+                    StepName="unzipping_payload",
+                    StepStdout="Payload unzipped!",
+                    StepSuccess=True
+                ))
+
             name = format(self.get_parameter("output")) if self.get_parameter("output") != "" else "SomeFile"
             if self.get_parameter("loader") == "control":
                 extension = ".cpl"
@@ -225,33 +272,19 @@ class NimSyscallPackerWrapper(PayloadType):
             if self.get_parameter("Caro-Kann") and not (self.get_parameter("QueueApc") or self.get_parameter("localCreateThread") or self.get_parameter("CallbackExecute") or self.get_parameter("remoteinject")):
                 resp.build_stderr = "Caro-Kann need to be use with QueueApc or localCreateThread or CallbackExecute or remoteinject!"
                 return resp
-            agent_build_path = tempfile.TemporaryDirectory(suffix=self.uuid).name
             #
-            # shutil to copy payload files over
-            copy_tree(str(self.agent_code_path), agent_build_path)
             #
-            # dummy input file
-            working_path = "{}/SomeFile.{}".format(agent_build_path, self.get_parameter("input-file-extension"))
+            # working_path = "{}/SomeFile.{}".format(agent_build_path, self.get_parameter("input-file-extension"))
             temp_input_file = "../SomeFile.{}".format(self.get_parameter("input-file-extension"))
-            with open(str(working_path), "wb") as f:
-                f.write(base64.b64decode(self.wrapped_payload))
-            # if working_path is not null, then we have a file to work with
-            if not os.path.exists(working_path):
-                resp.build_stderr = "Failed to find the payload to wrap!"
-                return resp
-            await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
-                PayloadUUID=self.uuid,
-                StepName="getting_agent_payload",
-                StepStdout="We found the payload to wrap!",
-                StepSuccess=True
-            ))
+            #
             # with open(str(working_path), "rb") as f:
             #     header = f.read(2)
             #     if header == b"\x4d\x5a":  # if PE file
             #         resp.build_stderr = "Supplied payload is a PE instead of raw shellcode."
             #         return resp
             #
-            command = "cd ./NimSyscallPacker && mkdir shared && nim c -d:noRES NimSyscallLoader.nim && ./NimSyscallLoader ".format(agent_build_path,agent_build_path)
+            # command = "cd ./NimSyscallPacker && mkdir shared && nim c -d:noRES NimSyscallLoader.nim && ./NimSyscallLoader ".format(agent_build_path,agent_build_path)
+            command = "cd ./NimSyscallPacker && mkdir shared && nim c -d:noRES NimSyscallLoader.nim && ./NimSyscallLoader "
 
             command += "--file {} {}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}".format(
                 #
